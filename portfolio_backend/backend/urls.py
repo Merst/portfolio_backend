902c0425fc0fd57000138b90f8d60df9dee3d386
Merst <noxlittner@gmail.com>
2024-10-14 19:21:35 -0400

    Skill-detail bugfix

--- portfolio_backend/backend/urls.py
+++ portfolio_backend/backend/urls.py
@@ -7,6 +7,9 @@ from backend import views
 router = DefaultRouter()
 router.register(r'projects', views.ProjectViewSet, basename='project')
 router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'skills', views.SkillViewSet, basename='skill')
+router.register(r'category', views.CategoryViewSet, basename='Category')
+
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
--- portfolio_backend/backend/views.py
+++ portfolio_backend/backend/views.py
@@ -1,8 +1,7 @@
-from backend.models import Project
+from backend.models import Project, Skill, Category
 from django.contrib.auth.models import User
-from backend.serializers import ProjectSerializer, UserSerializer
+from backend.serializers import ProjectSerializer, UserSerializer, SkillSerializer, CategorySerializer
 from rest_framework import generics
-
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -12,7 +11,7 @@ from rest_framework.reverse import reverse
 def api_root(request, format=None):
     return Response({
         'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
+        'projects': reverse('project-list', request=request, format=format)
     })
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -21,6 +20,20 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class SkillViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
     """
